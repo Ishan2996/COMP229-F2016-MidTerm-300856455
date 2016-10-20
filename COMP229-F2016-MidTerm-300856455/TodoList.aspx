7471268a494f85848eae52f8ddc69bf92b07e022
300856455 <300856455@PR-A313-06>
2016-10-19 20:33:38 -0400

    Table created

--- COMP229-F2016-MidTerm-300856455/TodoList.aspx
+++ COMP229-F2016-MidTerm-300856455/TodoList.aspx
@@ -1,5 +1,24 @@
 ﻿<%@ Page Title="Todo List" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TodoList.aspx.cs" Inherits="COMP229_F2016_MidTerm_300856455.TodoList" %>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-     
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <h1 class="h6"><b>Todo List</b></h1>
+
+            <asp:GridView ID="TodoGridView" runat="server" AutoGenerateColumns="false"
+                CssClass="table table-bordered table-striped table-hover">
+                <Columns>
+                    <asp:BoundField DataField="TodoDescription" HeaderText="ToDo Tasks" Visible="true" />
+                    <asp:BoundField DataField="TodoNotes" HeaderText="Notes" Visible="true" />
+                    <asp:BoundField DataField="Completed" HeaderText="Completed" Visible="true" />
+
+                </Columns>
+            </asp:GridView>
+            </div>
+        </div>
+    </div>
 </asp:Content>
+
+
+
--- COMP229-F2016-MidTerm-300856455/TodoList.aspx.cs
+++ COMP229-F2016-MidTerm-300856455/TodoList.aspx.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using COMP229_F2016_MidTerm_300856455.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
 namespace COMP229_F2016_MidTerm_300856455
 {
     public partial class TodoList : System.Web.UI.Page
@@ -12,8 +15,34 @@ namespace COMP229_F2016_MidTerm_300856455
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            // if loading the page for the first time
+            // populate the Todo table grid
+            if (!IsPostBack)
+            {
+                // Get the Todo data
+                this.gettodo();
+            }
+        }
 
+        private void gettodo()
+        {
+            // connect to EF DB
+            using (TodoContext db = new TodoContext())
+            {
+                // query the Todo Table using EF and LINQ
+                var todos = (from alltodos in db.Todos
+                             select alltodos);
+
+                // bind the result to the Todo GridView
+                TodoGridView.DataSource = todos.ToList();
+                TodoGridView.DataBind();
+            }
         }
 
     }
+
+    /// <summary>
+    /// This method gets the todo data from the DB
+    /// </summary>
+    
 }
